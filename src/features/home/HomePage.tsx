37e5285ecee3962a5303a4e4a9467f9e0982bb9e
tiedsandi <fachransandi@gmail.com>
2025-07-18 06:58:58 +0700

    feat: create simple homepage

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -1,3 +1,20 @@
+import { Link } from "react-router";
+
 export default function HomePage() {
-  return <h1>Home</h1>;
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold mb-4 text-white">
+        Welcome to Contact Management
+      </h1>
+      <p className="text-lg mb-8 text-gray-300">
+        Manage your contacts easily and efficiently.
+      </p>
+      <Link
+        to={"dashboard"}
+        className="px-8 py-3 text-base rounded-lg bg-blue-600 text-white shadow-md hover:bg-blue-700 transition"
+      >
+        Go to Dashboard
+      </Link>
+    </div>
+  );
 }
